d2ec90dc5d3a3ca044bc6de189c397b888413b43
리트리버 <qor8917@gmail.com>
2024-01-24 19:21:46 +0400

    window 에러 고치는중

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -1,7 +1,5 @@
-const IS_SERVER = typeof window === 'undefined';
 export default function getURL(path: string) {
-  const baseURL = IS_SERVER
-    ? process.env.NEXT_PUBLIC_SITE_URL!
-    : window.location.origin;
+  const baseURL = process.env.NEXT_PUBLIC_SITE_URL!;
+
   return new URL(path, baseURL).toString();
 }
